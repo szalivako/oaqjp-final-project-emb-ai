c4cc7de151816360b2ce4521de8d9d02f7360c37
szalivako <szalivako@gmail.com>
2025-08-27 06:53:17 -0400

    Formatted output and added dominant emotion detection

--- emotion_detection.py
+++ emotion_detection.py
@@ -14,5 +14,23 @@ def emotion_detector(text_to_analyze):
     # Make the POST request
     response = requests.post(url, headers=headers, json=input_json)
 
-    # Return the text attribute from the response JSON
-    return response.json()
+    # Convert response to JSON
+    response_dict = response.json()
+
+    # Extract emotions
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+
+    # Build dictionary
+    output = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+    }
+
+    # Find dominant emotion
+    dominant_emotion = max(output, key=output.get)
+    output['dominant_emotion'] = dominant_emotion
+
+    return output
